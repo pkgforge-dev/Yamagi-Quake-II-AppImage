122fd3d82a374cb97740133f6404e2b02241faf5
Link4Electronics <link4electronics@gmail.com>
2026-02-24 00:45:32 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/yamagi-quake2 /usr/bin/yamagi-q2ded /usr/lib/yamagi-quake2/*
+quick-sharun /usr/bin/yamagi-quake2 /usr/bin/yamagi-q2ded /usr/lib/yamagi-quake2/* /usr/lib/yamagi-quake2/baseq2/*
 
 # Additional changes can be done in between here
 
