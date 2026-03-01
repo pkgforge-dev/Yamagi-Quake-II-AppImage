e2a6b9638942b483e44642a1c8f22b6f6b60902f
Link4Electronics <link4electronics@gmail.com>
2026-03-01 18:39:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,16 +5,17 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/yamagi-quake2.png
+export ICON=https://raw.githubusercontent.com/yquake2/yquake2/refs/heads/master/stuff/icon/Quake2.svg
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/lib/yamagi-quake2/*
-
-# Additional changes can be done in between here
+quick-sharun ./AppDir/bin/quake2 ./AppDir/bin/* /usr/lib/libopenal.so*
+echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
+# this app has problems with other locales breaking physics
+echo 'LC_ALL=C.UTF-8' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
