998220102d839d6051266d892dce36fe540c4402
Link4Electronics <link4electronics@gmail.com>
2026-03-01 18:43:19 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,26 +6,29 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    libdecor \
-    openal   \
-    sdl3
+pacman -Syu --noconfirm openal sdl3
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
+echo "Building Yamagi Quake II..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/yquake2/yquake2"
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
-    package=yamagi-quake2-git
+    echo "Making nightly build of Yamagi Quake II..."
+    echo "---------------------------------------------------------------"
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone --depth 1 "$REPO" ./yquake2
 else
-    package=yamagi-quake2
+    echo "Making stable build of Yamagi Quake II..."
+    echo "---------------------------------------------------------------"
+    VERSION=$(git ls-remote --tags --refs --sort='v:refname' "$REPO" "refs/tags/QUAKE2_[0-9]*" | tail -n1 | cut -d/ -f3)
+    git clone --branch "$VERSION" --single-branch --depth 1 "$REPO" ./yquake2
 fi
-make-aur-package "$package"
-pacman -Q "$package" | awk '{print $2; exit}' > ~/version
+echo "${VERSION#QUAKE2_}" | tr '_' '.' > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./yquake2
+make -j$(nproc) WITH_RPATH=no WITH_SYSTEMWIDE=yes
+mv -v release/quake2 release/q2ded release/*.so release/baseq2 ../AppDir/bin
