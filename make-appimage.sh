89a0ad8ed5513cbb5a4a3f147bd60f156287bf13
Link4Electronics <link4electronics@gmail.com>
2026-08-19 04:08:05 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,8 +15,6 @@ export DEPLOY_VULKAN=1
 quick-sharun ./AppDir/bin/quake2 ./AppDir/bin/*
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
