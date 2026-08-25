8ebdcedadd963302df0777932031716de407fb24
Link4Electronics <link4electronics@gmail.com>
2026-08-25 19:03:00 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,13 +21,30 @@ get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
-if [ "${DEVEL_RELEASE-}" = 1 ]; then
-    package=yamagi-quake2-git
-else
-    package=yamagi-quake2
-fi
-make-aur-package "$package"
-pacman -Q "$package" | awk '{print $2; exit}' > ~/version
+#if [ "${DEVEL_RELEASE-}" = 1 ]; then
+#    package=yamagi-quake2-git
+#else
+#    package=yamagi-quake2
+#fi
+#make-aur-package "$package"
+#pacman -Q "$package" | awk '{print $2; exit}' > ~/version
 
 mkdir -p ./AppDir/bin
 mv -v /usr/lib/yamagi-quake2/* ./AppDir/bin
+
+echo "Building Yamagi Quake II..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/yquake2/yquake2"
+if [ "${DEVEL_RELEASE-}" = 1 ]; then
+    echo "Making nightly build of Yamagi Quake II..."
+    echo "---------------------------------------------------------------"
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone "$REPO" ./yquake2
+else
+    echo "Making stable build of Yamagi Quake II..."
+    echo "---------------------------------------------------------------"
+    VERSION=$(git ls-remote --tags --refs --sort='v:refname' "$REPO" "refs/tags/ra*" | tail -n1 | cut -d/ -f3)
+    git clone --branch "$VERSION" --single-branch "$REPO" ./yquake2
+fi
+echo "$VERSION" > ~/version
+
