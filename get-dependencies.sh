66c1b8297e94ef27636ce98be810f4eb89ea4c79
Link4Electronics <link4electronics@gmail.com>
2026-08-25 19:35:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,24 +14,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-#if [ "${DEVEL_RELEASE-}" = 1 ]; then
-#    package=yamagi-quake2-git
-#else
-#    package=yamagi-quake2
-#fi
-#make-aur-package "$package"
-#pacman -Q "$package" | awk '{print $2; exit}' > ~/version
-
-#mkdir -p ./AppDir/bin
-#mv -v /usr/lib/yamagi-quake2/* ./AppDir/bin
-
 echo "Building Yamagi Quake II..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/yquake2/yquake2"
@@ -43,12 +25,9 @@ if [ "${DEVEL_RELEASE-}" = 1 ]; then
 else
     echo "Making stable build of Yamagi Quake II..."
     echo "---------------------------------------------------------------"
-    #VERSION=$(git ls-remote --tags --refs --sort='v:refname' "$REPO" "refs/tags/ra*" | tail -n1 | cut -d/ -f3)
-    #git clone --branch "$VERSION" --single-branch "$REPO" ./yquake2
     VERSION=$(git ls-remote --tags --refs --sort='v:refname' "$REPO" "refs/tags/QUAKE2_[0-9]*" | tail -n1 | cut -d/ -f3)
     git clone --branch "$VERSION" --single-branch "$REPO" ./yquake2
 fi
-#echo "$VERSION" > ~/version
 echo "${VERSION#QUAKE2_}" | tr '_' '.' > ~/version
 
 mkdir -p ./AppDir/bin
