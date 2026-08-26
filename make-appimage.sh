a84ee06eeb5ded19ffefd926ee2f16bdbf0240fa
Link4Electronics <link4electronics@gmail.com>
2026-08-26 05:57:44 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,7 +12,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/quake2 ./AppDir/bin/*
+quick-sharun ./AppDir/bin/quake2 ./AppDir/bin/* /usr/lib/libopenal.so*
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 # this app has problems with other locales breaking physics
 echo 'LC_ALL=C.UTF-8' >> ./AppDir/.env
